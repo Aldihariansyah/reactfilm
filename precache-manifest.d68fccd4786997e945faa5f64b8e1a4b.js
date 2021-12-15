a637a03eba7944b36623430becfc6ffaff27a1bc
Aldihariansyah <aldibrian30@gmail.com>
2021-12-15 13:44:20 +0700

    Updates

---
+++ precache-manifest.d68fccd4786997e945faa5f64b8e1a4b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cabcca9c79bb675a03ee22ebe7373c",
+    "url": "/reactfilm/index.html"
+  },
+  {
+    "revision": "16aca32fe9cf63219125",
+    "url": "/reactfilm/static/css/2.abed5bb4.chunk.css"
+  },
+  {
+    "revision": "8a75df9550194c4dadc6",
+    "url": "/reactfilm/static/css/main.ee73a393.chunk.css"
+  },
+  {
+    "revision": "16aca32fe9cf63219125",
+    "url": "/reactfilm/static/js/2.08699b17.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/reactfilm/static/js/2.08699b17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a75df9550194c4dadc6",
+    "url": "/reactfilm/static/js/main.85f7e024.chunk.js"
+  },
+  {
+    "revision": "a099fdc7b250b2e59bfc",
+    "url": "/reactfilm/static/js/runtime-main.7e6b7a24.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/reactfilm/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/reactfilm/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/reactfilm/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "f86d9bf34b5da8610c6a218e96667b68",
+    "url": "/reactfilm/static/media/react-bootstrap-carousel.f86d9bf3.svg"
+  }
+]);
